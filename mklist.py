9799a26dc8cbed148a5fd3aab420558718eed6cc
Kirubakaran Shanmugam <kribakarans@gmail.com>
2024-05-25 06:52:46 +0530

    Alpha sort file list
    Modernized list and selected item

--- mklist.py
+++ mklist.py
@@ -4,7 +4,7 @@ import os
 import json
 
 html_dir = 'html'
-files = [f for f in os.listdir(html_dir) if f.endswith('.html')]
+files = sorted([f for f in os.listdir(html_dir) if f.endswith('.html')])
 
 with open('files.js', 'w') as f:
     f.write('const files = ' + json.dumps(files) + ';')
