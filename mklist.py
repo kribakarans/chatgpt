2da0bc6e9d4eef1b0e3bd7406cb1b9c5a5a68a0b
Kirubakaran Shanmugam <kribakarans@gmail.com>
2024-05-25 06:58:40 +0530

    Removed file extenstion in the list

--- mklist.py
+++ mklist.py
@@ -4,7 +4,7 @@ import os
 import json
 
 html_dir = 'html'
-files = sorted([f for f in os.listdir(html_dir) if f.endswith('.html')])
+files = sorted([os.path.splitext(f)[0] for f in os.listdir(html_dir) if f.endswith('.html')])
 
 with open('files.js', 'w') as f:
     f.write('const files = ' + json.dumps(files) + ';')
