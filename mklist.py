0eaf460fc0f9abfe8edc48d898608165c4df56ab
Kirubakaran Shanmugam <kribakarans@gmail.com>
2024-05-25 06:50:17 +0530

    Initial commit

---
+++ mklist.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+
+import os
+import json
+
+html_dir = 'html'
+files = [f for f in os.listdir(html_dir) if f.endswith('.html')]
+
+with open('files.js', 'w') as f:
+    f.write('const files = ' + json.dumps(files) + ';')
